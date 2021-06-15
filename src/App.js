92744a63d9d0c1718e1aa416d5073cc809f92a46
Squinky <hey@squinky.me>
2021-06-15 14:40:45 -0400

    protocol list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import React from 'react';
 import './App.css';
+
+class Protocol extends React.Component {
+	render() {
+		return (
+			<div className="Protocol">
+				<p className="Protocol-text">{this.props.text}</p>
+				<div className="Protocol-buttons">
+					<button className="Protocol-upvote">👍</button>
+					<button className="Protocol-veto">❌</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+function ProtocolList(props) {
+	const protocols = props.protocols;
+	const listItems = protocols.map((protocol) =>
+		<Protocol key={protocol} text={protocol} />
+	);
+	return (
+		<div className="ProtocolList">
+			{listItems}
+		</div>
+	);
+}
+
+const protocols = [
+	"Here is a protocol",
+	"Here is another protocol",
+	"Here is yet another protocol"
+]
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-container">
-				<div className="Protocol">
-					<p className="Protocol-text">butts</p>
-					<div className="Protocol-buttons">
-						<button className="Protocol-upvote">👍</button>
-						<button className="Protocol-veto">❌</button>
-					</div>
-				</div>
+				<ProtocolList protocols={protocols} />
 			</header>
 		</div>
 	);
