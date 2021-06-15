177c9c1b724e6733cc2eed4a303fef9530067ee5
Squinky <hey@squinky.me>
2021-06-15 12:50:00 -0400

    protocol css layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-container">
-        butts
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-container">
+				<div className="Protocol">
+					<p className="Protocol-text">butts</p>
+					<div className="Protocol-buttons">
+						<button className="Protocol-upvote">👍</button>
+						<button className="Protocol-veto">❌</button>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
