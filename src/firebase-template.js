83ae1642b782383529261d69a1c533f15d60e786
Squinky <hey@squinky.me>
2021-06-25 15:03:53 -0400

    template

--- src/firebase-template.js
+++ src/firebase-template.js
@@ -2,12 +2,12 @@
 
 import firebase from 'firebase'
 const config = {
-    apiKey: "PASTE_YOUR_API_KEY_HERE",
-    authDomain: "protocol-integration.firebaseapp.com",
-    databaseURL: "https://protocol-integration-default-rtdb.firebaseio.com",
-    projectId: "protocol-integration",
-    storageBucket: "protocol-integration.appspot.com",
-    messagingSenderId: "897368366871"
+    apiKey: "YOUR DATA HERE",
+    authDomain: "YOUR DATA HERE",
+    databaseURL: "YOUR DATA HERE",
+    projectId: "YOUR DATA HERE",
+    storageBucket: "YOUR DATA HERE",
+    messagingSenderId: "YOUR DATA HERE"
 };
 firebase.initializeApp(config);
 export default firebase;
